46c6f41f70352bb41b70a824eb3c93b76b2d20b5
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-08-12 21:46:13 +0530

    Update TopoSort.java

--- TopologicalSort/TopoSort.java
+++ TopologicalSort/TopoSort.java
@@ -1,1 +1,30 @@
 
+import java.util.*;
+
+class Solution {
+    private static void dfs(int node, int[] vis, Stack<Integer> stack, ArrayList<ArrayList<Integer>> adj) {
+        vis[node] = 1;
+        for (int it : adj.get(node)) {
+            if (vis[it] == 0) {
+                dfs(it, vis, stack, adj);
+            }
+        }
+        stack.push(node);
+    }
+
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) {
+        int[] vis = new int[V];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < V; i++) {
+            if (vis[i] == 0) {
+                dfs(i, vis, stack, adj);
+            }
+        }
+        int[] ans = new int[V];
+        int i = 0;
+        while (!stack.isEmpty()) {
+            ans[i++] = stack.pop();
+        }
+        return ans;
+    }
+}
