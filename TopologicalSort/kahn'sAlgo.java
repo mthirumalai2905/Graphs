7fc1d7daedbb6bc477f553879ed873543c3371ff
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-08-21 18:25:32 +0530

    Update kahn'sAlgo.java

--- TopologicalSort/kahn'sAlgo.java
+++ TopologicalSort/kahn'sAlgo.java
@@ -1,46 +1,38 @@
-import java.util.*;
-
-public class Graph {
-    private Map<Integer, List<Integer>> adjList;
-    
-    public Graph(int vertices){
-        adjList = new HashMap<>();
-        for(int i = 0; i < vertices; i++){
-            adjList.put(i, new ArrayList<>());
+class Solution
+{
+    //Function to return list containing vertices in Topological order. 
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+        // add your code here
+        int indegree[] = new int[V];
+        for(int i  = 0; i < V; i++){
+            for(int it: adj.get(i)){
+                indegree[it]++;
+            }
         }
-    }
-
-    public void addEdge(int src, int dest){
-        adjList.get(src).add(dest);
-    }
-
-    public void bfs(int start){
-        boolean[] vis = new boolean[adjList.size()];
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        vis[start] = true;
-
-        while(!queue.isEmpty()){
-            int node = queue.poll();
-            System.out.print(node + " ");
-
-            for(int ngbr : adjList.get(node)){
-                if(!vis[ngbr]){
-                    queue.add(ngbr);
-                    vis[ngbr] = true;
+        
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < V; i++){
+            if(indegree[i] == 0){
+                q.offer(i);
+            }
+        }
+        
+        int topo[] = new int[V];
+        int i = 0;
+        while(!q.isEmpty()){
+            int node = q.peek();
+            q.poll();
+            topo[i++] = node;
+            
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.add(it);
                 }
             }
         }
-    }
-
-    public static void main(String[] args){
-        Graph graph = new Graph(6);
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
-        graph.addEdge(1, 3);
-        graph.addEdge(1, 4);
-        graph.addEdge(2, 5);
-
-        graph.bfs(0);
+        return topo;
     }
 }
