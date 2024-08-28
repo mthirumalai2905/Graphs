0484214a64ea637af6d071312f52958737847262
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2024-08-28 12:30:16 +0530

    Update DijkstraAlgo.java

--- ShortestPath/DijkstraAlgo.java
+++ ShortestPath/DijkstraAlgo.java
@@ -1,1 +1,44 @@
+class Solution
+{
+    //Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
+    {
+        // Write your code here
+        PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingInt(pair -> pair.distance));
+        
+        int[] dist = new int[V];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[S] = 0;
+        
+        pq.add(new Pair(S, 0));
+        
+        while(!pq.isEmpty()){
+            Pair current = pq.poll();
+            int node = current.vertex;
+            int nodeDist = current.distance;
+            
+            for(ArrayList<Integer> edge : adj.get(node)){
+                int ngbr = edge.get(0);
+                int weight = edge.get(1);
+                
+                if(nodeDist + weight < dist[ngbr]){
+                    dist[ngbr] = nodeDist + weight;
+                    pq.add(new Pair(ngbr, dist[ngbr]));
+                }
+            }
+        }
+        
+        return dist;
+    }
+    static class Pair{
+        int vertex;
+        int distance;
+        
+        Pair(int vertex, int distance){
+            this.vertex = vertex;
+            this.distance = distance;
+        }
+    }
+}
 
